b6f42f36cc644b8caf612dce07e7f513e61a8241
josephloochunhoe <josephloo0407@gmail.com>
2020-09-09 19:12:36 +0800

    Test code to build basic Next.js web app

---
+++ pages/Contact.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+import ApolloClient from 'apollo-boost'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Just some text from contacts page</h1>
+            </div>
+        )
+    }   
+}
+export default Contact;
